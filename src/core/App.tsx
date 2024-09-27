ced123e86e5d8cc5873f1b057af6870869cb141d
Marcin Izdebski <mizdebski123@gmail.com>
2024-09-27 13:53:00 +0200

    Initial commit

---
+++ src/core/App.tsx
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div className="p-20">
+      hejeczka
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,tsx,ts}",
+    "./node_modules/react-tailwindcss-datepicker/dist/index.esm.js"
+  ],
+  theme: {
+    colors: {
+
+    },
+  },
+  plugins: [],
+}
+
