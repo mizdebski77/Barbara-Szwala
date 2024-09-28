2af08774826ab001166f10a8d3a8b947ebf8ce93
Marcin Izdebski <mizdebski123@gmail.com>
2024-09-28 11:33:49 +0200

    Animations to hero added

--- src/features/Home/Sections/hero.tsx
+++ src/features/Home/Sections/hero.tsx
@@ -2,31 +2,54 @@ import React from 'react';
 import hero from '../../../common/Images/hero.jpg';
 import logo from '../../../common/Images/BrightLogo.svg';
 import { Link } from 'react-router-dom';
+import { motion } from "framer-motion"
 
 export const Hero = () => {
     return (
         <div
-            className="hero min-h-screen"
+            className="hero min-h-screen overflow-hidden"
             style={{
                 backgroundImage: `url(${hero})`,
             }}>
             <div className="hero-overlay bg-opacity-80"></div>
 
             <div className=" grid md:gap-8 gap-4 backdrop-blur-sm lg:py-32 py-12 w-full md:px-12 px-4">
-                <div className='flex items-center justify-center md:gap-8 gap-4'>
+                <motion.div
+                    initial={{ y: '-30%', opacity: 0 }}
+                    whileInView={{ y: 0, opacity: 1 }}
+                    viewport={{ once: true }}
+                    transition={{ duration: 0.7 }}
+                    className='flex items-center justify-center md:gap-8 gap-4'>
                     <img src={logo} alt='Logo' className='md:max-w-20 max-w-12 w-full' />
-                    <h1 className='lg:text-8xl md:text-5xl text-2xl text-bright text-center tracking-widest m-0'>Szwala Barbara</h1>
-                </div>
-                <p className='md:text-2xl text-lg text-bright opacity-80 text-center'>
+                    <h1
+
+                        className='lg:text-8xl md:text-5xl text-2xl text-bright text-center tracking-widest m-0'
+                    >Szwala Barbara
+                    </h1>
+                </motion.div>
+                <motion.p
+                    initial={{ scale: 0.8, opacity: 0 }}
+                    whileInView={{ scale: 1, opacity: 1 }}
+                    viewport={{ once: true }}
+                    transition={{ duration: 0.7, }}
+                    className='md:text-2xl text-lg text-bright opacity-80 text-center'>
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi adipisci delectus, quod,
                     rem dolorem soluta sunt sapiente nemo reprehenderit odit, rerum non fuga minus dolores voluptatibus at.
                     Provident, exercitationem optio.
-                </p>
+                </motion.p>
 
-                <Link to="/Kontakt" className="m-auto group relative overflow-hidden rounded-xl shadow-3xl bg-bright md:px-12 px-8 md:py-3 py-2 text-dark transition-all duration-300 ease-out hover:bg-gradient-to-r hover:from-bright hover:dark hover:ring-1  hover:ring-offset-2">
+                <motion.a
+                    initial={{ y: '30%', opacity: 0 }}
+                    whileInView={{ y: 0, opacity: 1 }}
+                    viewport={{ once: true }}
+                    transition={{ duration: 0.7 }}
+                    href="/Kontakt"
+                    className="m-auto group relative overflow-hidden rounded-xl shadow-3xl bg-bright md:px-12 px-8 md:py-3 py-2 text-dark transition-all duration-300 ease-out hover:bg-gradient-to-r hover:from-bright hover:dark hover:ring-1  hover:ring-offset-2">
                     <span className="ease absolute right-0 -mt-12 h-32 w-12 translate-x-12 rotate-12 transform bg-dark opacity-10  transition-all duration-700 group-hover:-translate-x-80"></span>
                     <span className="relative md:text-xl ">Kontakt</span>
-                </Link>
+                </motion.a>
+
+
             </div>
 
         </div>
