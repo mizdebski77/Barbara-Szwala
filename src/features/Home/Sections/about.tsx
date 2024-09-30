e170e58407d58bb2f4c9a88dd6a1e98a33043349
Marcin Izdebski <mizdebski123@gmail.com>
2024-09-30 10:24:25 +0200

    Framer motion to about section added

--- src/features/Home/Sections/about.tsx
+++ src/features/Home/Sections/about.tsx
@@ -6,21 +6,43 @@ export const About = () => {
     return (
         <div className='grid md:gap-8 gap-4 md:pb-20 pb-12'>
             <motion.h2
-                initial={{ x: '-5%', opacity: 0 }}
-                whileInView={{ x: 0, opacity: 1 }}
+                initial={{ y: '-50%', opacity: 0 }}
+                whileInView={{ y: 0, opacity: 1 }}
                 viewport={{ once: false }}
                 transition={{ duration: 1 }}
                 className='md:text-7xl text-3xl text-center tracking-widest'>
                 Kilka słów o mnie
             </motion.h2 >
 
-            <div className='grid grid-cols-2 gap-2 max-w-screen-lg mx-auto mt-8 items-center'>
-                <img src={Barbara} alt='Barbara' className='m-auto shadow-2xl border-4' />
+            <div className='grid md:grid-cols-2 gap-4 max-w-screen-lg mx-auto mt-8 items-center px-2'>
+                <motion.img
+                    initial={{ x: '-10%', opacity: 0 }}
+                    whileInView={{ x: 0, opacity: 1 }}
+                    viewport={{ once: false }}
+                    transition={{ duration: 1 }}
+                    src={Barbara}
+                    alt='Barbara'
+                    className='m-auto shadow-2xl border-4 max-w-80 w-full' />
                 <div>
-                    <div className='grid gap-8 items-center justify-center'>
-                        <p className='md:text-5xl text-2xl text-center tracking-wide'>Barbara Szwala</p>
-                        <p className='text-2xl opacity-75 text-justify'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium libero vitae, harum provident laudantium natus officia voluptatibus ad, voluptate nisi voluptas repellendus voluptates non amet architecto in cum consectetur deleniti.</p>
-                        <p className='text-2xl opacity-75 text-justify'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium libero vitae, harum provident laudantium natus officia voluptatibus ad, voluptate nisi voluptas repellendus voluptates non amet architecto in cum consectetur deleniti.</p>
+                    <div className='grid md:gap-8 gap-4 items-center justify-center'>
+                        <motion.p
+                            initial={{ x: '10%', opacity: 0 }}
+                            whileInView={{ x: 0, opacity: 1 }}
+                            viewport={{ once: false }}
+                            transition={{ duration: 1 }}
+                            className='md:text-5xl text-2xl text-center tracking-wide'>Barbara Szwala</motion.p>
+                        <motion.p
+                            initial={{ y: '10%', opacity: 0 }}
+                            whileInView={{ y: 0, opacity: 1 }}
+                            viewport={{ once: false }}
+                            transition={{ duration: 1 }}
+                            className='md:text-2xl text-lg opacity-75 text-justify'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium libero vitae, harum provident laudantium natus officia voluptatibus ad, voluptate nisi voluptas repellendus voluptates non amet architecto in cum consectetur deleniti.</motion.p>
+                        <motion.p
+                            initial={{ y: '10%', opacity: 0 }}
+                            whileInView={{ y: 0, opacity: 1 }}
+                            viewport={{ once: false }}
+                            transition={{ duration: 1, delay: 0.2 }}
+                            className='md:text-2xl text-lg opacity-75 text-justify'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium libero vitae, harum provident laudantium natus officia voluptatibus ad, voluptate nisi voluptas repellendus voluptates non amet architecto in cum consectetur deleniti.</motion.p>
                     </div>
                 </div>
 
