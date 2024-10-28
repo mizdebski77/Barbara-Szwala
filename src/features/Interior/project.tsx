21e5863f945a9fc450e6c9c60b90575629210787
Marcin Izdebski <mizdebski123@gmail.com>
2024-10-28 15:06:59 +0100

    Full screen prev added

--- src/features/Interior/project.tsx
+++ src/features/Interior/project.tsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { motion } from "framer-motion"
 import bg from '../../common/Images/contact.jpg';
 import logo from '../../common/Images/BrightLogo.svg';
+import { Navigation } from 'swiper/modules'
+import 'swiper/css';
+import 'swiper/css/navigation';
+import { AiOutlineClose } from 'react-icons/ai'
+import { Swiper, SwiperSlide } from "swiper/react";
+
 
 declare const require: {
     context(path: string, recursive: boolean, regExp: RegExp): {
@@ -10,11 +16,52 @@ declare const require: {
     };
 };
 
+const openImage = {
+    opacity: 1,
+    scale: 1
+};
+
+const closeImage = {
+    opacity: 0,
+    scale: 0
+};
+
+const openWrapper = {
+    opacity: 1,
+};
+
+const closeWrapper = {
+    opacity: 0,
+};
+
+
 export const Project = () => {
 
+    const [fullScreen, setFullScreen] = useState<boolean>(false);
+    // eslint-disable-next-line
+    const [fullScreenSrc, setFullScreenSrc] = useState<string>("");
+    const [currentImageIndex, setCurrentImageIndex] = useState(-1);
+
+
+    const smallImagesFolder = require.context('../../common/Images/Projects', true, /\.png$/);
     const largeImagesFolder = require.context('../../common/Images/Projects', true, /\.png$/);
+    const smallImageKeys = smallImagesFolder.keys();
     const largeImageKeys = largeImagesFolder.keys();
 
+    const openFullScreen = (index: number) => {
+        setCurrentImageIndex(index);
+        setFullScreenSrc(largeImagesFolder(largeImageKeys[index]) as string);
+        setFullScreen(true);
+    };
+
+    const closeFullScreen = () => {
+        setFullScreen(false);
+        setCurrentImageIndex(-1);
+    };
+
+    const handleImageContextMenu = (e: React.MouseEvent<HTMLImageElement, MouseEvent>) => {
+        e.preventDefault();
+    };
     return (
         <section>
             <div className='hero min-h-screen grid gap-8'
@@ -51,11 +98,58 @@ export const Project = () => {
                     {largeImageKeys.map((imageKey, index) => (
                         <img
                             src={largeImagesFolder(imageKey) as string}
-
-                            key={index} alt="Project" className="max-w-screen-lg m-auto" />
+                            onClick={() => openFullScreen(index)}
+                            key={index} alt="Project"
+                            className="max-w-screen-lg m-auto cursor-pointer duration-300 hover:opacity-90 " />
                     ))}
                 </div>
             </div>
+
+            {fullScreen && (
+                <motion.div
+                    initial={closeWrapper}
+                    animate={fullScreen ? openWrapper : closeWrapper}
+                    transition={{ duration: 0.5 }}
+                    className="fixed top-0 left-0 bottom-0 right-0 z-10 w-full h-full bg-dark bg-opacity-95 p-10 sm:p-3 m-auto"
+                >
+                    <Swiper
+                        centeredSlides
+                        slidesPerView={1}
+                        modules={[Navigation]}
+                        spaceBetween={100}
+                        loop={false}
+                        navigation
+                        grabCursor
+                        initialSlide={currentImageIndex}
+                        className="w-full h-full flex justify-center items-center 
+                        [&_.swiper-button-prev]:!text-bright 
+                        [&_.swiper-button-next]:!text-bright"
+                    >
+                        {largeImageKeys.map((imageKey) => (
+                            <SwiperSlide
+                                key={imageKey}
+                                className="flex justify-center items-center w-[calc(100%-40px)] h-[calc(100%-40px)]"
+                            >
+                                <motion.img
+                                    src={largeImagesFolder(imageKey)}
+                                    alt="large"
+                                    className="max-h-full m-auto max-w-full select-none"
+                                    onContextMenu={handleImageContextMenu}
+                                    initial={closeImage}
+                                    animate={fullScreen ? openImage : closeImage}
+                                    transition={{ duration: 0.5 }}
+                                />
+                            </SwiperSlide>
+                        ))}
+                    </Swiper>
+                    <button
+                        onClick={() => closeFullScreen()}
+                        className="fixed z-10 top-5 right-20 text-bright hover:scale-125 transition-transform duration-300 sm:right-2.5 bg-transparent border-none"
+                    >
+                        <AiOutlineClose size={35} />
+                    </button>
+                </motion.div>
+            )}
         </section>
 
     );
